ae610a121427af6648c38ccf69db4045dc095ffa
cvalenzuela <cvalenzuela@cia-des-01>
2012-08-27 10:28:03 -0400

    Cambio del mesaje de salida

--- Prueba123/src/py/una/pol/git/prueba/GitTest.java
+++ Prueba123/src/py/una/pol/git/prueba/GitTest.java
@@ -2,7 +2,7 @@ package py.una.pol.git.prueba;
 
 public class GitTest {
 	public static void main(String[] args) {
-	    System.out.println("Git is fun");
+	    System.out.println("Git is cool");
 	  }
 
 }
